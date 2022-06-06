6c5e9802fc7676291cbe927218cfb0a2fb1a02eb
AliFJ <haitex.lion@gmail.com>
2022-06-06 10:37:40 -0700

    update items

---
+++ script.js
@@ -0,0 +1,26 @@
+let darkmode = false
+
+function changecolor(){
+    if(darkmode){
+        document.getElementById('container').style.backgroundColor= "hsl(0, 0%, 40%)";
+        document.getElementById('box').style.backgroundColor= 'hsl(0, 0%, 10%)';
+        document.getElementById("text").style.color = 'white'
+        document.getElementById('btn').style.backgroundColor="hsl(212, 45%, 89%)"
+        document.getElementById('btn').style.color="hsl(0, 0%, 40%)"
+        document.getElementById('imgQr').src = 'Qrd.png'
+        document.getElementById('btn').innerHTML= 'Light Mode'
+        darkmode = false
+    }else{
+        darkmode = true
+        document.getElementById('container').style.backgroundColor= "hsl(212, 45%, 89%)";
+        document.getElementById('box').style.backgroundColor= 'hsl(0, 0%, 100%)';
+        document.getElementById("text").style.color = 'black'
+        document.getElementById('btn').style.backgroundColor="hsl(0, 0%, 40%)"
+        document.getElementById('btn').style.color="hsl(212, 45%, 89%)"
+        document.getElementById('imgQr').src = 'QR.png'
+        document.getElementById('btn').innerHTML= 'Dark Mode'
+    }
+
+
+
+}
